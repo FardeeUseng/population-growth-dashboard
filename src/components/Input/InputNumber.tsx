17f5a6527781f6101054c38b0f1920384b51cfa0
D <fardee.tat@gmail.com>
2025-05-20 04:59:01 +0700

    feat: inputText, inputNumber, inputTextArea common

---
+++ src/components/Input/InputNumber.tsx
@@ -0,0 +1,19 @@
+import type { InputNumberProps } from "antd";
+
+export default function InputNumber({ ...rest }: InputNumberProps) {
+  return (
+    <InputNumber
+      {...rest}
+      formatter={
+        rest.formatter
+          ? rest.formatter
+          : (value) => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+      }
+      parser={
+        rest.parser
+          ? rest.parser
+          : (value) => value?.replace(/\$\s?|(,*)/g, "") as unknown as number
+      }
+    />
+  );
+}
---
+++ src/components/Input/InputTextArea.tsx
@@ -0,0 +1,24 @@
+import { ChangeEvent } from "react";
+import { Input } from "antd";
+import { escapeHtmlEntities } from "@/utils/escapeHtml";
+import type { TextAreaProps } from "antd/es/input";
+
+export default function InputTextArea({ onChange, ...rest }: TextAreaProps) {
+  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    const escapeValue = escapeHtmlEntities(event.target.value);
+
+    if (onChange) {
+      const newEvent: ChangeEvent<HTMLTextAreaElement> = {
+        ...event,
+        target: {
+          ...event.target,
+          value: escapeValue,
+        },
+      };
+
+      onChange(newEvent);
+    }
+  };
+
+  return <Input.TextArea onChange={handleChange} {...rest} />;
+}
