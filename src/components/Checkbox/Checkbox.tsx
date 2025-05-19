9e9e069b8faba993547f946ebf5711399ccde6b5
D <fardee.tat@gmail.com>
2025-05-20 04:58:02 +0700

    refactor: checkbox and group common

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,6 @@
+import { Checkbox as AntdCheckbox } from 'antd';
+import type { CheckboxProps } from 'antd';
+
+export default function Checkbox({ ...rest }: CheckboxProps) {
+  return <AntdCheckbox {...rest} />
+}
--- src/components/CheckboxGroup/index.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import React from 'react';
-import { Checkbox } from 'antd';
-
-export default function CheckboxGroup({ ...rest }) {
-  return <Checkbox.Group {...rest} />
-}
